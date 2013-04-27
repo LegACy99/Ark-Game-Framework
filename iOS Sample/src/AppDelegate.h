481b5dc9951f344b4d6ebde1d96058278484dbd3
Raka Mahesa <the_legacy99@hotmail.com>
2013-04-27 17:31:21 +0700

    Added initial files for iOS sample project.

---
+++ iOS Sample/src/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iOS Sample
+//
+//  Created by LegACy on 4/27/13.
+//  Copyright (c) 2013 LegACy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iOS Sample/src/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS Sample
+//
+//  Created by LegACy on 4/27/13.
+//  Copyright (c) 2013 LegACy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
